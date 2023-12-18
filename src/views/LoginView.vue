db35edaae687965c25f1b91e18b487199bea14d3
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2023-12-18 14:41:01 +0100

    adding route and page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="container min-vh-100 d-flex justify-content-center align-items-center"
+  >
+    <div class="card">
+      <div class="card-header d-flex justify-content-center">
+        <p class="text-title">YourCount</p>
+      </div>
+      <div class="card-body">
+        <div class="col-auto">
+          <label class="sr-only text-regular" for="inlineFormInput"
+            >email</label
+          >
+          <input
+            type="email"
+            class="form-control mb-2"
+            id="emailInput"
+            placeholder="email"
+          />
+          <label class="sr-only text-regular" for="inlineFormInput"
+            >password</label
+          >
+          <input
+            type="password"
+            class="form-control"
+            id="passwordInput"
+            placeholder="password"
+          />
+        </div>
+      </div>
+      <div class="card-footer">
+        <div class="d-flex justify-content-around">
+          <button
+            type="button"
+            class="btn bg-black text-white"
+            style="font-family: 'Gustavo'"
+          >
+            Connexion
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
