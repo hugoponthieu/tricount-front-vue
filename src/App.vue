4217c839696aa6005653dbf0e33ccdcca15204f6
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2023-12-25 17:55:03 +0100

    login cookie token

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <router-view :key="login" v-if="true" />
-  <div class="row min-vh-100" v-if="false">
+  <router-view :key="login" v-if="!isAuth" />
+  <div class="row min-vh-100" v-if="isAuth">
     <div class="col-3 bg-black"><TheRightSideNav /></div>
     <div class="col-9"><router-view :key="route.path" /></div>
   </div>
@@ -9,7 +9,7 @@
 <script setup>
 import TheRightSideNav from "@/components/TheRightSideNav.vue";
 import { useRoute } from "vue-router";
-// import { store } from "./store";
-// let isAuth = store;
+import { store } from "./store";
+let isAuth = store;
 const route = useRoute();
 </script>
