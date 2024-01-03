bcc4a639c7993ff2db47c744cddca690b6bc18a7
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-01-03 14:09:29 +0100

    server conf

---
+++ src/components/TotalDepenseUser.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div
+      class="modal-dialog modal-dialog-centered modal-dialog-scrollabl8 modal-lg"
+    >
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel"></h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">...</div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-5">
+    <div class="row">
+      <div class="container medium-text">Your total</div>
+      <div class="d-flex justify-content-center logo">
+        54.34 €<button
+          class="col-3 btn"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Voir le detail
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
