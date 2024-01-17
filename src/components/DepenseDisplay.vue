a72061bdad32f4ab2f9955faf6d3de21e133bafe
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-01-17 14:10:33 +0100

    wip depense edition

--- src/components/DepenseDisplay.vue
+++ src/components/DepenseDisplay.vue
@@ -11,14 +11,69 @@
           v-for="depense in depenses"
           :key="depense.id"
         >
-          <div class="row row-cols-1">
-            <div class="col simple-text">
-              {{ depense.description }}
+          <div
+            class="modal fade"
+            :id="depense + String(depense.id)"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div
+              class="modal-dialog modal-dialog-centered modal-dialog-scrollabl8 modal-lg"
+            >
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">
+                    Edition d'une dépense
+                  </h1>
+                  <button
+                    type="button"
+                    @click="console.log('hey')"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">...</div>
+
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    @click="console.log('hey')"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Fermer
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    data-bs-dismiss="modal"
+                    v-on:click="postGroup()"
+                  >
+                    Créer
+                  </button>
+                </div>
+              </div>
             </div>
-            <div class="col">{{ depense.utilisateur }}</div>
+          </div>
+          <div class="row flex align-content-center">
+            <div class="col">
+              <button
+                type="button"
+                @click="console.log('hey')"
+                class="btn-close"
+                data-bs-toggle="modal"
+                :data-bs-target="dep"
+              ></button>
+            </div>
+            <div class="col">
+              <p class="simple-text">{{ depense.description }}</p>
+              {{ depense.utilisateur }}
+            </div>
           </div>
 
-          <p class="little-text">{{ depense.montant }}</p>
+          <p class="little-text">{{ depense.montant }} €</p>
         </div>
       </div>
     </div>
@@ -57,13 +112,14 @@ const getDepenses = async () => {
     return data;
   } catch (error) {
     console.error("Error fetching expense:", error);
-    throw error; // Re-throw the error to propagate it further if needed
+    throw error;
   }
 };
 
 onMounted(async () => {
   try {
     depenses.value = await getDepenses();
+    console.log(depenses.value);
   } catch (error) {
     console.error("Error setting groups:", error);
   }
