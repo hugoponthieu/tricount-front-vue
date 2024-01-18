679cf2bde85cdd8bfcbfbe8818816d95aa6b5e89
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-01-18 16:08:58 +0100

    fix env

--- src/main.js
+++ src/main.js
@@ -5,10 +5,9 @@ import "./assets/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap";
-
 let app = createApp(App);
 const initApp = async () => {
-  app.provide("ip", process.env.ADRESS);
+  app.provide("ip", process.env.VUE_APP_ADDRESS);
 };
 await initApp();
 app.use(router);
