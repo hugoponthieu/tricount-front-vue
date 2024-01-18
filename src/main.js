d7742c6c1194d5aad60a081afc64383ae4cab70a
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-01-18 01:30:27 +0100

    variable ip

--- src/main.js
+++ src/main.js
@@ -1,28 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-// import BootstrapVue from "bootstrap-vue";
 import "./assets/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap";
 
-let app = createApp(App); //.use(router).use(BootstrapVue).mount("#app");
-// const getIPAddress = async () => {
-//   try {
-//     const response = await fetch("https://api64.ipify.org?format=json");
-//     const data = await response.json();
-//     return data.ip;
-//   } catch (error) {
-//     console.error("Erreur lors de la récupération de l'adresse IP :", error);
-//     return "localhost"; // Utilisez une valeur par défaut en cas d'erreur
-//   }
-// };
-
-// Utilisez une fonction asynchrone pour récupérer l'adresse IP avant de monter l'application
+let app = createApp(App);
 const initApp = async () => {
-  // const ip = await getIPAddress();
-  app.provide("ip", "localhost");
+  app.provide("ip", process.env.ADRESS);
 };
 await initApp();
 app.use(router);
